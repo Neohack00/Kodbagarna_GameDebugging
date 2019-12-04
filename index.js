34bd89fb68563cbd4be25873b083332f74b8c155
Neohack00 <neo@bylock.se>
2019-12-04 17:30:43 +0100

    Changing code position

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var app = express();
 var Lport = 1000;
 var Hport = process.env.PORT;
 
+app.use(express.static('game'));
 app.get('/' , function(req, res){
     res.sendFile(__dirname +'/game/index.html');
 })
-app.use(express.static('game'));
+
 
 var server = app.listen(Hport , function(){
     console.log("Server running at " + Hport);
