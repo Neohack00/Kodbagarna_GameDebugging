a93eaab95ca213169cdfe0c04324e57521c815b3
Neohack00 <neo@bylock.se>
2019-12-04 16:52:25 +0100

    fixed server

--- index.js
+++ index.js
@@ -11,8 +11,8 @@
  */
  
 // Parameters
-var sitePath = process.argv[2] || ".";
-var port = process.env.PORT;
+// var sitePath = process.argv[2] || ".";
+var port = process.env.port;
 
 // Libraries
 var express = require('express');
@@ -23,11 +23,13 @@ app.use(function(req, res, next) {
     console.log(req.url);
     next();
 });
+// app.use(express.static('game'))
 
 // Start server
-console.log(sitePath);
-console.log("Starting server in: " + __dirname + '/' + sitePath);
-app.use(express.static(__dirname + '/' + sitePath));
+// console.log(sitePath);
+console.log("Starting server in: " + __dirname + '/' );
+app.use(express.static(__dirname + '/game'));
+
 app.listen(port, function() { 
     console.log("Server running at: http://localhost:" + port)
 });
